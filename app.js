996aed6c8f26bf91750282d10d92b91733c4a877
fmahmoudifar <fmahmoudifar@gmail.com>
2022-08-21 21:52:20 +0300

    1 file modified

--- app.js
+++ app.js
@@ -5,24 +5,25 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    let temp = 0;
-    let tempArr =[];
-    for(let i=0; i<nums.length; i++) {
-        console.log(nums[i])
-       for(let j=0; j<nums[i].length; j++) { console.log(nums[j])
-          temp = nums[i] +nums[j];
-           if (temp === target) {
-          return nums.indexOf(i)
-               tempArr.push(indexOf(nums[i]));
-               tempArr.push(nums[j]);
-               //return tempArr;
-               break;
-              } 
-           
-       } 
-       } console.log(tempArr)
-    
-    console.log(nums) 
-   // return nums
+var twoSum = function (nums, target) {
+  let temp = 0;
+  let tempArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i]);
+    for (let j = 0; j < nums[i].length; j++) {
+      console.log(nums[j]);
+      temp = nums[i] + nums[j];
+      if (temp === target) {
+        return nums.indexOf(i);
+        tempArr.push(indexOf(nums[i]));
+        tempArr.push(nums[j]);
+        //return tempArr;
+        break;
+      }
+    }
+  }
+  console.log(tempArr);
+
+  console.log(nums);
+  // return nums
 };
